4ee1c6072780b5b6629d4163e25424a4c2a8e440
nusrathumaira12 <nusrathum31@gmail.com>
2025-04-06 19:02:41 +0600

    more react core concepts

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+import Counter from './counter';
+import Batsman from './batsman';
+import './App.css'
+
+
+function App() {
+ 
+  function handleClick(){
+alert('I am clicked')
+  }
+
+const handleClick3 = () => {
+  alert('clicked3')
+}
+const handleAdd5 = (num) => {
+  const newNum = num + 5;
+  alert(newNum)
+}
+
+ 
+  return (
+    <>
+    
+
+      <h3>Vite + React</h3>
+
+      <Batsman></Batsman>
+      <Counter></Counter>
+
+
+      {/* <button onclick="handleClick()">Click  me</button> */}
+      <button onClick={handleClick}>Click  me</button>
+      <button onClick={function handleClick2(){
+        alert('clicked2')
+      }}>Click  me2</button>
+     
+     <button onClick={handleClick3}>Click me 3</button>
+     <button onClick={() => alert('click 4')}>Click me 4</button>
+     
+     <button onClick={() => handleAdd5(10)}>Click Add 5</button>
+    </>
+  )
+}
+
+export default App
